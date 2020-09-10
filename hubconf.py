2455fbb6b79fc31185f10d03b522f0eeb1a618a0
Paschalis Bizopoulos <pbizopoulos@iti.gr>
2020-09-10 15:06:53 +0300

    spelling mistake

--- hubconf.py
+++ hubconf.py
@@ -22,6 +22,6 @@ def segmentation_model(pretrained=True, experiment_name='lung-segmentation', arc
         architecture = PSPNet
     model = architecture(encoder, encoder_weights=encoder_weights, activation='sigmoid', in_channels=1).to('cpu')
     if pretrained:
-        checkpoint = f'https://github.com/pbizopoulos/comprehensive-comparison-of-deep-learning-models-for-lung-and-covid-19-lesion-segmentation-in-ct/releases/download/v1/{experiment_name}-{architecture_name}-{encoder}-{endocer_weights}.pt'
+        checkpoint = f'https://github.com/pbizopoulos/comprehensive-comparison-of-deep-learning-models-for-lung-and-covid-19-lesion-segmentation-in-ct/releases/download/v1/{experiment_name}-{architecture_name}-{encoder}-{encoder_weights}.pt'
         model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False, map_location='cpu'))
     return model
