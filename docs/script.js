d966bb88adaa6ec1e8c377c91302f8f25434d5cc
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-06-10 13:50:45 +0300

    .

--- docs/script.js
+++ docs/script.js
@@ -5,18 +5,23 @@ const imageInputContext = imageInputCanvas.getContext("2d");
 const maskOutputCanvas = document.getElementById("mask-output-canvas");
 const maskOutputContext = maskOutputCanvas.getContext("2d");
 const modelDownloadDiv = document.getElementById("model-download-div");
-const modelDownloadProgress = document.getElementById("model-download-progress");
+const modelDownloadProgress = document.getElementById(
+	"model-download-progress",
+);
 const modelSelect = document.getElementById("model-select");
 const image = new Image();
 let model;
 image.crossOrigin = "anonymous";
 image.onload = imageOnLoad;
-image.src = "https://raw.githubusercontent.com/pbizopoulos/comprehensive-comparison-of-deep-learning-models-for-lung-and-covid-19-lesion-segmentation-in-ct/main/latex/python/assets/lung-segmentation-example-data.png";
+image.src =
+	"https://raw.githubusercontent.com/pbizopoulos/comprehensive-comparison-of-deep-learning-models-for-lung-and-covid-19-lesion-segmentation-in-ct/main/latex/python/assets/lung-segmentation-example-data.png";
 imageFileReader.onload = imageFileReaderOnLoad;
 inputFile.onchange = inputFileOnChange;
 
 function disableUI(argument) {
-	const nodes = document.getElementById("input-control-div").getElementsByTagName("*");
+	const nodes = document
+		.getElementById("input-control-div")
+		.getElementsByTagName("*");
 	for (let i = 0; i < nodes.length; i++) {
 		nodes[i].disabled = argument;
 	}
@@ -34,8 +39,23 @@ function imageFileReaderOnLoad() {
 }
 
 function imageOnLoad() {
-	imageInputContext.clearRect(0, 0, imageInputCanvas.width, imageInputCanvas.height);
-	imageInputContext.drawImage(image, 0, 0, image.width, image.height, 0, 0, imageInputCanvas.width, imageInputCanvas.height);
+	imageInputContext.clearRect(
+		0,
+		0,
+		imageInputCanvas.width,
+		imageInputCanvas.height,
+	);
+	imageInputContext.drawImage(
+		image,
+		0,
+		0,
+		image.width,
+		image.height,
+		0,
+		0,
+		imageInputCanvas.width,
+		imageInputCanvas.height,
+	);
 	predictView();
 }
 
@@ -62,17 +82,37 @@ function predictView() {
 	tf.tidy(() => {
 		let fromPixels = tf.browser.fromPixels(imageInputCanvas);
 		const originalShape = fromPixels.shape.slice(0, 2);
-		fromPixels = tf.image.resizeNearestNeighbor(fromPixels, [model.inputs[0].shape[2], model.inputs[0].shape[3]]);
-		let pixels = fromPixels.slice([0, 0, 2]).squeeze(-1).expandDims(0).expandDims(0);
+		fromPixels = tf.image.resizeNearestNeighbor(fromPixels, [
+			model.inputs[0].shape[2],
+			model.inputs[0].shape[3],
+		]);
+		let pixels = fromPixels
+			.slice([0, 0, 2])
+			.squeeze(-1)
+			.expandDims(0)
+			.expandDims(0);
 		pixels = pixels.mul(3 / 255);
 		pixels = pixels.sub(1.5);
 		const mask = model.predict(pixels);
 		let maskToPixels = mask.squeeze(0).squeeze(0);
 		const alphaTensor = tf.tensor([0.3]);
 		const alphaChannel = alphaTensor.where(maskToPixels.greaterEqual(0.5), 0);
-		maskToPixels = tf.stack([maskToPixels, tf.zerosLike(maskToPixels), tf.zerosLike(maskToPixels), alphaChannel], -1);
+		maskToPixels = tf.stack(
+			[
+				maskToPixels,
+				tf.zerosLike(maskToPixels),
+				tf.zerosLike(maskToPixels),
+				alphaChannel,
+			],
+			-1,
+		);
 		maskToPixels = tf.image.resizeNearestNeighbor(maskToPixels, originalShape);
-		maskOutputContext.clearRect(0, 0, imageInputCanvas.width, imageInputCanvas.height);
+		maskOutputContext.clearRect(
+			0,
+			0,
+			imageInputCanvas.width,
+			imageInputCanvas.height,
+		);
 		tf.browser.toPixels(maskToPixels, maskOutputCanvas);
 	});
 }
