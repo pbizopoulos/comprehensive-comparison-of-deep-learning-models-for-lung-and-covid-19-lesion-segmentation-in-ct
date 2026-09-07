61834190477020389458e0a39ff1c492326e398e
Paschalis Bizopoulos <pbizop@gmail.com>
2026-09-07 16:22:37 +0300

    .

---
+++ checks/default_coverage/default.nix
@@ -0,0 +1,26 @@
+{ inputs, pkgs, ... }:
+let
+  checkName = baseNameOf ./.;
+  packageDrv = inputs.self.packages.${pkgs.stdenv.system}.${packageName};
+  packageName = pkgs.lib.removeSuffix "_coverage" checkName;
+  pythonEnv = packageDrv.python.withPackages (
+    _:
+    packageDrv.propagatedBuildInputs
+    ++ [
+      packageDrv.python.pkgs.pytest
+      packageDrv.python.pkgs.pytest-cov
+    ]
+  );
+in
+pkgs.runCommand checkName
+  {
+    nativeBuildInputs =
+      packageDrv.nativeBuildInputs ++ packageDrv.propagatedBuildInputs ++ [ pythonEnv ];
+    src = ../.. + "/packages/${packageName}";
+  }
+  ''
+    export HOME="$(mktemp -d)"
+    mkdir -p "$out/html"
+    cd "$out"
+    PACKAGE_E2E_EXECUTABLE="${packageDrv}/bin/${packageName}" python -m pytest -p no:cacheprovider --cov="$src" --cov-report "html:$out/html" "$src/main.py"
+  ''
--- packages/default/default.nix
+++ packages/default/default.nix
@@ -32,7 +32,10 @@ python.pkgs.buildPythonPackage {
       cp -R prm/ "$out/bin/"
     fi
   '';
-  meta.mainProgram = pname;
+  meta = {
+    description = "A Python package.";
+    mainProgram = pname;
+  };
   nativeBuildInputs = nativeDeps;
   passthru.python = python;
   propagatedBuildInputs = pythonDeps;
