9951b300e1196d3ff410d913cbb3781c00f9b930
Paschalis Bizopoulos <pbizop@gmail.com>
2026-09-16 15:17:07 +0300

    .

--- packages/default/default.nix
+++ packages/default/default.nix
@@ -37,10 +37,7 @@ python.pkgs.buildPythonPackage {
     mainProgram = pname;
   };
   nativeBuildInputs = nativeDeps;
-  passthru = {
-    inherit python;
-    canonicalization.tests = [ "Main." ];
-  };
+  passthru.python = python;
   propagatedBuildInputs = pythonDeps;
   pyproject = false;
   src = ./.;
